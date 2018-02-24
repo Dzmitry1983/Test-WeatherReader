5546565c9f0a685e6ec9ce0a8741b87e0babe5c1
Dzmitry Kudrashou <dmitriy1983@gmail.com>
2018-02-23 21:16:17 -0600

    add worldcities info csv file

--- modules/cities_informer.js
+++ modules/cities_informer.js
@@ -1,9 +1,17 @@
+const fs = require('fs');
+
+//city,city_ascii,lat,lng,pop,country,iso2,iso3,province
+
+const csv_file = '../csv/simplemaps-worldcities-basic.csv';
 
 exports.getCityInfoByName = getCityInfoByName;
 
+//var contents = fs.readFileSync(csv_file);
+
+//simplemaps-worldcities-basic.csv
 /*
  * Get main information about city by city name 
  */
 function getCityInfoByName(city_name) {
-	
+//	console.log(contents);
 }
